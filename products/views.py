5e000805625311a2c1406df0dee201d6b851b353
daisybutler <daisybutler16@gmail.com>
2021-04-09 11:20:57 +0000

    Completed nav dropdown functionality.

--- products/views.py
+++ products/views.py
@@ -1,31 +1,40 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and searching queries. """
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria.")
+                messages.error(
+                    request, "You didn't enter any search criteria.")
                 return redirect(reverse('products'))
-            
-            # | creates the either/or functionality and the i before contains makes it case insensitive.
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
 
+            # | creates the either/or functionality and the i before contains makes it case insensitive.
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
