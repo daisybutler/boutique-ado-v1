e83e3fcb27de511b0a8db807e3c0fc9c895cfd30
daisybutler <daisybutler16@gmail.com>
2021-05-06 11:10:11 +0000

    Fix deploy failure attempt 4.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Retrieving from gitpod variables in gitpod settings
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
